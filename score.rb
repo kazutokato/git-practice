8c1a2f8d308b7517c03ae37867e452ad8eea052b
EC2 Default User <ec2-user@ip-172-31-93-199.ec2.internal>
2020-10-22 15:53:50 +0000

    [Start] 10/23

--- score.rb
+++ score.rb
@@ -1,9 +1,13 @@
 score = 70
 
-if (score >= 50 || score <= 100) && score >= 80
+if score >= 50 || score && score >= 80
   puts "得点は50点以上または100点以下で、かつ80以上です。"
-end
+elsif score <= 100 && score && score >= 80
+  puts "特典はまたは100点以下で、かつ80点以上です。"
+end 
   
-if score >= 50 || (score <= 100 && scpre >= 80)
+if score >= 50 
   puts "得点は50点以上、または80点以上100点以下です。"
+elsif score <= 100 && score >= 80
+  puts "特典は50点以上、または80点以上100点以下です。"
 end
